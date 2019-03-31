d0d374bd7980e5b44e8e68abec47ff76e4ed9b36
Kjell Jansson <kjell.jansson@gubboit.se>
2019-03-31 16:03:06 +0200

    Normalization added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ tf.ENV.set('WEBGL_PACK', false); // fix Edge problem
 // as MNIST data.
 var fillStyle = "#000000"; //  black
 var strokeStyle = "#FFFFFF"; //  white
+var lineWidth = 5;
 
 var rimage = new Image(); // image saved for rotation
 var rangle = 0; // angle for rotatation
@@ -14,6 +15,7 @@ var ctx = canvas.getContext('2d');
 var crect = canvas.getBoundingClientRect();
 
 var inter; // 360 running
+var eraser = 0;
 
 var model_name = "white";
 var histo_toggle = 0;
@@ -38,11 +40,13 @@ document.getElementById("up_btn").addEventListener("click", up);
 document.getElementById("down_btn").addEventListener("click", down);
 document.getElementById("predict_btn").addEventListener("click", predict_btn);
 document.getElementById("toggle_btn").addEventListener("click", toggle);
+document.getElementById("norm_btn").addEventListener("click", normalize);
 document.getElementById("zoom_in_btn").addEventListener("click", zoom_in);
 document.getElementById("zoom_out_btn").addEventListener("click", zoom_out);
 document.getElementById("rright_btn").addEventListener("click", rright);
 document.getElementById("rleft_btn").addEventListener("click", rleft);
 document.getElementById("r360_btn").addEventListener("click", r360);
+document.getElementById("eraser_btn").addEventListener("click", toggle_eraser);
 
 document.getElementById('canvas').addEventListener("touchstart", setTouchPos, true);
 document.getElementById('canvas').addEventListener("touchmove", touchDraw, true);
@@ -76,24 +80,40 @@ function draw(e) {
   // mouse left button must be pressed 
   if (e.buttons !== 1) return;
   ctx.beginPath(); // begin
-  ctx.lineWidth = 5;
+  ctx.lineWidth = lineWidth;
+  if (eraser) {
+	ctx.lineWidth = 2*lineWidth;
+	ctx.strokeStyle = fillStyle;
+  }
   ctx.lineCap = 'round';
   ctx.moveTo(pos.x, pos.y); // from
   setPosition(e);
   ctx.lineTo(pos.x, pos.y); // to
   ctx.stroke(); // draw it!
+  if (eraser) {
+	ctx.lineWidth = lineWidth;
+	ctx.strokeStyle = strokeStyle;
+  }
   reset_rot();
 }
 
 function touchDraw(e) {
   ctx.beginPath(); // begin
-  ctx.lineWidth = 5;
+  ctx.lineWidth = lineWidth;
+   if (eraser) {
+	ctx.lineWidth = 2*lineWidth;
+	ctx.strokeStyle = fillStyle;
+  } 
   ctx.lineCap = 'round';
   ctx.moveTo(pos.x, pos.y); // from
   setTouchPos(e);
   ctx.lineTo(pos.x, pos.y); // to
   ctx.stroke(); // draw it!
   event.preventDefault();
+  if (eraser) {
+	ctx.lineWidth = lineWidth;
+	ctx.strokeStyle = strokeStyle;
+  }
   reset_rot();
 }
 
@@ -103,7 +123,6 @@ function resize() {
 	crect = canvas.getBoundingClientRect();
 	reset_rot();
 }
-
 
 //--------------------- Presentation functions ---------------------------------------
 function clear() {
@@ -121,31 +140,42 @@ function toggle() {
 	load_model();
 }
 
+function toggle_eraser() {
+	if (eraser)
+		eraser = 0;
+	else
+		eraser = 1;
+}
+
 function right() {
-	var cData = ctx.getImageData(0, 0, canvas.width - 5, canvas.height);
-	ctx.putImageData(cData, 5, 0);
-	ctx.fillRect(0, 0, 5, canvas.height);	
+	const pos = 5;
+	var cData = ctx.getImageData(0, 0, canvas.width - pos, canvas.height);
+	ctx.putImageData(cData, pos, 0);
+	ctx.fillRect(0, 0, pos, canvas.height);	
 	reset_rot();
 }
 
 function left() {
-	var cData = ctx.getImageData(5, 0, canvas.width, canvas.height);
+	const pos = 5;
+	var cData = ctx.getImageData(pos, 0, canvas.width, canvas.height);
 	ctx.putImageData(cData, 0, 0);
-	ctx.fillRect(canvas.width - 5, 0, canvas.width, canvas.height);	
+	ctx.fillRect(canvas.width - pos, 0, canvas.width, canvas.height);	
 	reset_rot();
 }
 
 function up() {
-	var cData = ctx.getImageData(0, 5, canvas.width, canvas.height);
+	const pos = 5;
+	var cData = ctx.getImageData(0, pos, canvas.width, canvas.height);
 	ctx.putImageData(cData, 0, 0);
-	ctx.fillRect(0, canvas.height - 5, canvas.width, canvas.height);
+	ctx.fillRect(0, canvas.height - pos, canvas.width, canvas.height);
 	reset_rot();	
 }
 
 function down() {
-	var cData = ctx.getImageData(0, 0, canvas.width, canvas.height - 5);
-	ctx.putImageData(cData, 0, 5);
-	ctx.fillRect(0, 0, canvas.width, 5);	
+	const pos = 5;
+	var cData = ctx.getImageData(0, 0, canvas.width, canvas.height - pos);
+	ctx.putImageData(cData, 0, pos);
+	ctx.fillRect(0, 0, canvas.width, pos);	
 	reset_rot();
 }
 
@@ -194,7 +224,8 @@ function rleft() {
 }
 
 function r360() {
-	var r360_btn = document.getElementById('r360_btn')	
+	predict(0);
+	var r360_btn = document.getElementById('r360_btn');	
 	if (inter != null) {
 		clearInterval(inter);
 		r360_btn.innerHTML ='360';
@@ -223,6 +254,104 @@ function rotate(angle) {
 function reset_rot() {
 	rimage.src = canvas.toDataURL('image/png', 1.0); // save canvas as image
 	rangle = 0;
+}
+
+function normalize() {
+	var lx, ty, rx, by; // bounding rectangle
+	var fix, lix;
+	var fillpix = 255;
+	if (fillStyle.indexOf("00") > 0) fillpix = 0;
+	var cData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	var cdata = cData.data;
+
+	lx = getLx(cdata, fillpix, canvas.width, canvas.height);
+	if (lx == -1) return; // canvas is empty
+	rx = getRx(cdata, fillpix, canvas.width, canvas.height);
+	ty = getTy(cdata, fillpix, canvas.width, canvas.height);
+	by = getBy(cdata, fillpix, canvas.width, canvas.height);
+
+	draw_rect(lx - 2, ty - 2, rx + 2, by + 2, "#FF0000"); // bounding rectangle
+	setTimeout(function() {
+		draw_rect(lx - 2, ty - 2, rx + 2, by + 2, fillStyle);
+
+		// We have 200x200, MNIST is 28x28 where the margin is 4 => Our margin should be around 28.
+		var marg = 28;
+		var sw = rx + 1 - lx;
+		var sh = by + 1 - ty;
+		var prop = sw/sh;
+		var ax, ay; // actual x and y
+		var aw, ah; // actual with and height
+		if (sh > sw) {
+			ah = canvas.height - 2*marg;
+			ax = Math.floor(0.5*(canvas.width - prop*ah));
+			ay = marg;
+			aw = Math.floor(prop*(canvas.height - 2*marg));
+		} else {
+			aw = canvas.width - 2*marg;
+			ax = marg
+			ay = Math.floor(0.5*(canvas.height - aw/prop));
+			ah = Math.floor((canvas.width - 2*marg)/prop);		
+		}
+		ctx.drawImage(canvas, lx, ty, sw, sh, ax, ay, aw, ah);
+		
+		ctx.fillRect(0, 0, canvas.width, ay);
+		ctx.fillRect(0, ay + ah, canvas.width, canvas.height);
+		ctx.fillRect(0, 0, ax, canvas.height);
+		ctx.fillRect(ax + aw, 0, canvas.width, canvas.height);
+		
+		reset_rot();
+	}, 500); // end setTimeout
+}
+
+function getTy(cdata, fillpix, width, height) {
+	var fix;
+	for (fix=0; fix < cdata.length; fix+=4) {
+		if (cdata[fix] != fillpix) break;	
+	}
+	if (fix == cdata.length) return -1; // canvas is empty
+	return Math.floor(fix/(width*4));
+}
+
+function getBy(cdata, fillpix, width, height) {
+	var lix;
+	for (lix=cdata.length - 4; lix >= 0; lix-=4) {
+		if (cdata[lix] != fillpix) break;	
+	}
+	return Math.floor(lix/(width*4));
+}
+
+function getLx(cdata, fillpix, width, height) {
+	var fix=0;
+	var row, col;
+	for (col=0; col < width; col++) {
+		for (row=0; row < height; row++) {
+			fix = (row*width + col)*4;
+			if (cdata[fix] != fillpix) return col;		
+		}
+	}
+	return -1; // canvas is empty
+}
+
+function getRx(cdata, fillpix, width, height) {
+	var lix;
+	var row, col;
+	for (col=width-1; col >= 0; col--) {
+		for (row=height-1; row >= 0; row--) {
+			lix = (row*width + col)*4;
+			if (cdata[lix] != fillpix) return col;	
+		}
+	}
+	return -1;
+}
+
+function draw_rect(lx, ty, rx, by, color) {
+	var savStroke = ctx.strokeStyle;
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = color;
+	ctx.beginPath();
+	ctx.rect(lx, ty, rx - lx, by - ty);
+	ctx.stroke();
+	ctx.strokeStyle = savStroke;
 }
 
 function histo(prediction) {
@@ -283,9 +412,8 @@ function predict(sound) {
 	for (var i = 0; i < cdata.length; i += 4) { // to grayscale
       cdata[i] = (cdata[i] + cdata[i + 1] + cdata[i + 2]) / 3;
     }
-
 	var x = tf.browser.fromPixels(cData, 1).asType('float32'); // keep only one channel	
-	x = tf.image.resizeNearestNeighbor(x, [28, 28]);	// resize
+	x = tf.image.resizeNearestNeighbor(x, [28, 28]);	// resize, no normalization
 	x = x.expandDims();
 	x = x.div(255);
 	var prediction;
